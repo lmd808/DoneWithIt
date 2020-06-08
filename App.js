f6f52ce36f646bbfd34f0e24188593a781e94932
Laura DiTommaso <52080492+lmd808@users.noreply.github.com>
2020-06-08 19:54:13 -0400

    added listing page features and notes on adding styles withing safearea view

--- App.js
+++ App.js
@@ -3,11 +3,11 @@ import React from 'react';
 
 import Screen from './app/components/Screen'
 import ListItem from './app/components/ListItem';
-import Icon from './app/components/Icon';
-import AccountScreen from './app/screens/AccountScreen';
+
+import ListingScreen from './app/screens/ListingScreen';
 
 export default function App() {
 	return (
-		<AccountScreen/>
+		<ListingScreen/>
 	)
 }
--- app/components/Screen/Screen.js
+++ app/components/Screen/Screen.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { Text, StyleSheet, SafeAreaView } from 'react-native';
+import { Text, StyleSheet, SafeAreaView, View } from 'react-native';
 
 import Styles from './styles';
 
 function Screen({ children, style }) {
-	return <SafeAreaView style={[Styles.screen, style]}>{children}</SafeAreaView>;
+	return (
+		// safe area view does not support vertical padding as of 6/7/2019
+		// wrap children in view and send style props to View to force change
+		<SafeAreaView style={[Styles.screen, style]}>
+			<View style={style}>
+				{children}
+			</View>
+		</SafeAreaView>
+	);
 }
 export default Screen;
